632a99f4c5ca31569e3a54924a9e63e09ec8559a
p.bychkov <wononi02144@gmail.com>
2023-01-17 13:35:41 +0300

    webpack and add tag picture

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,68 @@
 // webpack.config.js
-const path = require('path')
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const optimization = () => {
+    const config = {};
+    if (isProd) {
+        config.minimizer = [new CssMinimizerPlugin(), new TerserWebpackPlugin()];
+    }
+    return config;
+};
+
+const excludeSourceMap = () => {
+    if (isProd) {
+        return 'bundle.js';
+    }
+};
 
 module.exports = {
-    entry: {
-        main: path.resolve(__dirname, './src/index.js'),
-    },
-}
-
-module.exports = {
-    // ...
-
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: ['./js/index.js', './style/style.scss'],
     output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: '[name].bundle.js',
+        filename: 'index.bundle.js',
+        path: path.resolve(__dirname, './assets/js')
     },
-}
-
-module.exports = {
-    // ...
-
+    optimization: optimization(),
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '../css/style.css'
+        }),
+        new webpack.SourceMapDevToolPlugin({
+            filename: '[file].map',
+            exclude: excludeSourceMap(),
+        }),
+    ],
     module: {
         rules: [
-            // JavaScript
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: ['babel-loader'],
+            },
+        ],
+        rules: [
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                            url: true,
+                            import: true,
+                        },
+                    },
+                    'postcss-loader',
+                    'sass-loader',
+                ],
             },
         ],
     }
