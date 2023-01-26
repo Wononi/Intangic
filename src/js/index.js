1e07f6b23e4eabc1de2770f637861f53edba4291
p.bychkov <wononi02144@gmail.com>
2023-01-26 19:38:29 +0300

    Обратная связь по правкам

--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,16 @@ import '../style/style.scss'
 // window.addEventListener('scroll', () => {
 //     document.querySelector('.sphere__background').style.transform = `scale(1.${pageYOffset / 100})`
 // })
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        console.log(entry)
+        if (!entry.isIntersecting) {
+            document.querySelector('.scroll__nav').classList.add('scroll__nav-show')
+        } else {
+            document.querySelector('.scroll__nav').classList.remove('scroll__nav-show')
+        }
+    });
+});
+
+observer.observe(document.querySelector('.navigation'))
