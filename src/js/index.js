dd4be3f8d1e337edd455cabe4c94c2f58e83ff5e
Petr Bychkov <wononi02144@gmail.com>
2023-03-07 16:06:01 +0300

    add animation second block on home page

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import './animate-block'
 import '../style/style.scss'
+import ScrollMagic from 'scrollmagic';
 
 // window.addEventListener('scroll', () => {
 //     document.querySelector('.sphere__background').style.transform = `scale(1.${pageYOffset / 100})`
@@ -23,10 +24,32 @@ const getHeightPicture = () => {
     return imac.clientHeight
 
 }
-
 setInterval(() => {
     imacWrapper.setAttribute('style', `height: ${getHeightPicture()}px`)
 }, 100)
+// анимация второго блока на главной странице
+var controller = new ScrollMagic.Controller();
 
+// build scene
+new ScrollMagic.Scene({
+    triggerElement: "#trigger",
+})
+  .setClassToggle("#animate", "visible") // add class to reveal
+  .addIndicators() // add indicators (requires plugin)
+  .addTo(controller);
 
+setTimeout(() => {
+    new ScrollMagic.Scene({
+        triggerElement: "#trigger",
+    })
+      .setClassToggle("#animateBGOne", "visible") // add class to reveal
+      .addIndicators() // add indicators (requires plugin)
+      .addTo(controller);
 
+    new ScrollMagic.Scene({
+        triggerElement: "#trigger",
+    })
+      .setClassToggle("#animateBGTwo", "visible") // add class to reveal
+      .addIndicators() // add indicators (requires plugin)
+      .addTo(controller);
+}, 4000)
